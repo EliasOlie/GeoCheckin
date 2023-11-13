06220bb551f3739d84fd9b8a6dfe0a37e4e728cd
EliasOlie <eliasvieira31@gmail.com>
2023-11-13 13:56:09 -0300

    Added CSRF token to forms

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import LoginForm from "@/components/Forms/LoginForm";
+import { getCsrfToken } from "next-auth/react";
+import type { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { useSession } from "next-auth/react";
 import Head from "next/head";
+import { useEffect } from "react";
 
-export default function Home() {
+import  { useRouter }  from "next/navigation"
+
+export default function Home({ csrfToken }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const { status } = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if(status === "authenticated") {
+      router.push("/dashboard")
+    }
+  } , [])
 
   return (
     <>
@@ -10,13 +25,21 @@ export default function Home() {
         <meta name="description" content="Faça checkin!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className=" flex min-h-screen flex-col items-center justify-center gap-4">
+      <main className="flex min-h-screen flex-col items-center justify-center gap-4">
         <div>
           <h1 className="text-2xl font-bold">Faça o login para continuar</h1>
           <p className="text-xm">É rapidinho!</p>
         </div>
-        <LoginForm />
+        <LoginForm csrfToken={csrfToken}/>
       </main>
     </>
   );
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  return {
+    props: {
+      csrfToken: await getCsrfToken(context),
+    },
+  }
 }
