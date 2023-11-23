fcaa5cfffb3da94f02dbf3b77728ee8edabdfda8
EliasOlie <eliasvieira31@gmail.com>
2023-11-23 09:46:19 -0300

    Debug

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useSession, signOut } from "next-auth/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { api } from "@/utils/api";
 import { QrReader } from 'react-qr-reader';
@@ -10,6 +10,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Dashboard() {
   const { status } = useSession()
+  const [ data, setData ] = useState<string>("Sem resultado")
   const router = useRouter()
   const getUserQuery = api.user.getUser.useQuery().data
 
@@ -29,15 +30,16 @@ export default function Dashboard() {
       <main className="min-w-full min-h-screen">
         <div className="flex items-center justify-between min-w-full px-4 mt-4">
          <h2 className="text-xl font-bold">Olá {getUserQuery?.name} 👋</h2>
-          <Button onClick={() => signOut()} className="bg-red-500">Sair</Button>
+          <Button className="bg-red-500">Sair</Button>
         </div>
         <div className="max-w-[50vw] mx-auto mt-4 border-2 border-black">
           <QrReader
           onResult={(resultado, error) => {
             if (!!resultado) {
-                if(resultado?.getText() === "checkin") {
-                  window.alert("checkin")
+              if(resultado.getText() === "checkin") {
+                  alert("Foi")
                 }
+              setData(resultado?.getText());
             }
 
             if (!!error) {
@@ -47,7 +49,7 @@ export default function Dashboard() {
             constraints={ { aspectRatio: 1/1, facingMode: "environment" } }
           />
         </div>
-      
+        {data} 
       </main>
       <BottomBar />
     </>
